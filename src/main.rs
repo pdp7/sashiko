c9053a79fc6db01284aa58134b7240b257c54774
Roman Gushchin <roman.gushchin@linux.dev>
2026-01-02 23:43:06 +0000

    Add CLI argument parsing with clap and set default log level to info
    Signed-off-by: Roman Gushchin <roman.gushchin@linux.dev>

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod nntp;
 mod patch;
 mod settings;
 
+use clap::Parser;
 use db::Database;
 use events::Event;
 use ingestor::Ingestor;
@@ -16,12 +17,27 @@ use settings::Settings;
 use std::sync::Arc;
 use tokio::sync::mpsc;
 use tracing::{error, info};
+use tracing_subscriber::{EnvFilter, fmt};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: Option<String>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Initialize tracing with EnvFilter
-    tracing_subscriber::fmt()
-        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+    // Parse command line arguments
+    let _cli = Cli::parse();
+
+    // Initialize tracing with EnvFilter, defaulting to "info" if RUST_LOG is not set
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info"));
+
+    fmt()
+        .with_env_filter(env_filter)
         .init();
 
     info!("Starting Sashiko...");
